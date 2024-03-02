db3e7ee1e071a09a809446c23c6bb7b429a0459f
adena-welt <vchristina122@gmail.com>
2024-03-02 18:32:59 -0300

    pasta Naive Bayes

---
+++ modeloNB.py
@@ -0,0 +1,77 @@
+import joblib
+import re
+from sklearn.feature_extraction.text import CountVectorizer
+
+'''import nltk
+nltk.download('stopwords')
+import nltk
+nltk.download('punkt')'''
+
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('portuguese'))
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+# Carregar o modelo treinado
+modelo_naive_bayes = joblib.load('Naive Bayes/modelo_naive_bayes.pkl')
+
+# Carregar o vetorizador usado durante o treinamento
+vectorizer = joblib.load('vectorizer.pkl')
+
+import re
+
+def remover_acentos(texto):
+    # Mapear caracteres acentuados para seus equivalentes não acentuados
+    mapa_acentos = {
+        'á': 'a', 'à': 'a', 'ã': 'a', 'â': 'a',
+        'é': 'e', 'ê': 'e', 'í': 'i', 'ó': 'o',
+        'õ': 'o', 'ô': 'o', 'ú': 'u', 'ç': 'c',
+        'Á': 'A', 'À': 'A', 'Ã': 'A', 'Â': 'A',
+        'É': 'E', 'Ê': 'E', 'Í': 'I', 'Ó': 'O',
+        'Õ': 'O', 'Ô': 'O', 'Ú': 'U', 'Ç': 'C'
+    }
+    # Substituir caracteres acentuados por seus equivalentes não acentuados
+    return ''.join(mapa_acentos.get(char, char) for char in texto)
+
+# Função para pré-processar a notícia
+def preprocessar_noticia(noticia):
+    # Converter para minúsculas
+    noticia = noticia.lower()
+    # Remover caracteres especiais, exceto letras com acento e números
+    noticia = re.sub(r'[^a-zA-Záàãâéêíóõôúç\s\d]', '', noticia)
+    # Remover acentos
+    noticia = remover_acentos(noticia)
+    # Remover números
+    noticia = re.sub(r'\b\d+\b', '', noticia)
+    # Tokenizar a notícia
+    tokens = word_tokenize(noticia)
+    # Remover stopwords
+    stop_words = set(stopwords.words('portuguese'))
+    tokens = [word for word in tokens if word not in stop_words]
+    # Juntar tokens de volta em uma string
+    noticia_processada = ' '.join(tokens)
+    return noticia_processada
+
+def prever_noticia(noticia):
+    # Pré-processar a notícia
+    noticia_processada = preprocessar_noticia(noticia)
+    # Vetorizar a notícia
+    noticia_vetorizada = vectorizer.transform([noticia_processada])
+    # Fazer a previsão usando o modelo treinado
+    previsao = modelo_naive_bayes.predict(noticia_vetorizada)
+    # Imprimir o texto pré-processado da notícia
+    '''print("\n\nTexto pré-processado da notícia:")
+    print(noticia_processada + "\n\n")'''
+    # Retornar o resultado da previsão
+    if previsao[0] == 'fake':
+        return "A notícia é falsa."
+    else:
+        return "A notícia é verdadeira."
+
+# Exemplo de uso
+noticia = input("\nDigite a notícia: ")
+resultado = prever_noticia(noticia)
+print("\n")
+print(resultado)
+
---
+++ naivebayes.py
@@ -0,0 +1,72 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import joblib
+
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+'''from sklearn.externals import joblib'''
+
+# Carregar o arquivo CSV
+df = pd.read_csv("base de dados/Fake.br-Corpus-master/preprocessed/pre-processed.csv")
+
+# Dividir os dados em conjuntos de treinamento e teste
+X = df['preprocessed_news']  # texto das notícias pré-processadas
+y = df['label']  # rótulos (fake ou true)
+
+# Dividir os dados em conjuntos de treinamento e teste
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Contar o número de exemplos de "fake" e "true" em cada conjunto de dados
+train_fake_count = (y_train == "fake").sum()
+train_true_count = (y_train == "true").sum()
+
+test_fake_count = (y_test == "fake").sum()
+test_true_count = (y_test == "true").sum()
+
+# Imprimir a contagem de exemplos de "fake" e "true" em cada conjunto de dados
+print("Contagem de exemplos no conjunto de treino:")
+print("Fake:", train_fake_count)
+print("True:", train_true_count)
+
+print("\nContagem de exemplos no conjunto de teste:")
+print("Fake:", test_fake_count)
+print("True:", test_true_count)
+
+# Vetorização do texto usando CountVectorizer
+vectorizer = CountVectorizer()
+X_train_vectorized = vectorizer.fit_transform(X_train)
+X_test_vectorized = vectorizer.transform(X_test)
+
+# Salvar o vetorizador
+joblib.dump(vectorizer, 'vectorizer.pkl')
+
+# Treinar o classificador Naive Bayes
+naive_bayes_classifier = MultinomialNB()
+naive_bayes_classifier.fit(X_train_vectorized, y_train)
+
+# Salvar o modelo treinado
+joblib.dump(naive_bayes_classifier, 'modelo_naive_bayes.pkl')
+
+# Fazer previsões
+predictions = naive_bayes_classifier.predict(X_test_vectorized)
+
+# Avaliar o desempenho do modelo
+accuracy = accuracy_score(y_test, predictions)
+print("Accuracy:", accuracy)
+
+report = classification_report(y_test, predictions)
+print("Classification Report:")
+print(report)
+
+# Plotar a matriz de confusão
+conf_mat = confusion_matrix(y_test, predictions)
+plt.figure(figsize=(8, 6))
+sns.heatmap(conf_mat, annot=True, fmt='d', cmap='Blues', cbar=False,
+            xticklabels=['True', 'Fake'], yticklabels=['True', 'Fake'])
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Confusion Matrix')
+plt.show()
