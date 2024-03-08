f8294e5b9004c43f9e009f7e335a38a5c272f468
adena-welt <vchristina122@gmail.com>
2024-03-07 22:54:11 -0300

    cv = 5, accuracy 0.82

--- naivebayes.py
+++ naivebayes.py
@@ -1,13 +1,11 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import joblib
-
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-'''from sklearn.externals import joblib'''
 
 # Carregar o arquivo CSV
 df = pd.read_csv("pre-processed.csv")
@@ -22,7 +20,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Contar o número de exemplos de "fake" e "true" em cada conjunto de dados
 train_fake_count = (y_train == "fake").sum()
 train_true_count = (y_train == "true").sum()
-
 test_fake_count = (y_test == "fake").sum()
 test_true_count = (y_test == "true").sum()
 
@@ -35,38 +32,45 @@ print("\nContagem de exemplos no conjunto de teste:")
 print("Fake:", test_fake_count)
 print("True:", test_true_count)
 
-# Vetorização do texto usando TfidfVectorizer
-tfidf_vectorizer = TfidfVectorizer()
-X_train_tfidf = tfidf_vectorizer.fit_transform(X_train)
-X_test_tfidf = tfidf_vectorizer.transform(X_test)
+# Vetorização do texto usando CountVectorizer
+vectorizer = CountVectorizer()
+X_train_vectorized = vectorizer.fit_transform(X_train)
+X_test_vectorized = vectorizer.transform(X_test)
 
-# Salvar o vetorizador TF-IDF
-joblib.dump(tfidf_vectorizer, 'tfidf_vectorizer.pkl')
+# Salvar o vetorizador
+joblib.dump(vectorizer, 'vectorizer.pkl')
 
-# Treinar o classificador Naive Bayes com os vetores TF-IDF
-naive_bayes_classifier_tfidf = MultinomialNB()
-naive_bayes_classifier_tfidf.fit(X_train_tfidf, y_train)
+# Treinar o classificador Naive Bayes
+naive_bayes_classifier = MultinomialNB()
+naive_bayes_classifier.fit(X_train_vectorized, y_train)
 
-# Salvar o modelo treinado com TF-IDF
-joblib.dump(naive_bayes_classifier_tfidf, 'modelo_naive_bayes_tfidf.pkl')
+# Salvar o modelo treinado
+joblib.dump(naive_bayes_classifier, 'modelo_naive_bayes.pkl')
 
-# Fazer previsões com TF-IDF
-predictions_tfidf = naive_bayes_classifier_tfidf.predict(X_test_tfidf)
+# Fazer previsões
+predictions = naive_bayes_classifier.predict(X_test_vectorized)
 
-# Avaliar o desempenho do modelo com TF-IDF
-accuracy_tfidf = accuracy_score(y_test, predictions_tfidf)
-print("Accuracy (TF-IDF):", accuracy_tfidf)
+# Avaliar o desempenho do modelo
+accuracy = accuracy_score(y_test, predictions)
+print("Accuracy:", accuracy)
 
-report_tfidf = classification_report(y_test, predictions_tfidf)
-print("Classification Report (TF-IDF):")
-print(report_tfidf)
+report = classification_report(y_test, predictions)
+print("Classification Report:")
+print(report)
 
-# Plotar a matriz de confusão com TF-IDF
-conf_mat_tfidf = confusion_matrix(y_test, predictions_tfidf)
+# Plotar a matriz de confusão
+conf_mat = confusion_matrix(y_test, predictions)
 plt.figure(figsize=(8, 6))
-sns.heatmap(conf_mat_tfidf, annot=True, fmt='d', cmap='Blues', cbar=False,
+sns.heatmap(conf_mat, annot=True, fmt='d', cmap='Blues', cbar=False,
             xticklabels=['True', 'Fake'], yticklabels=['True', 'Fake'])
 plt.xlabel('Predicted')
 plt.ylabel('Actual')
-plt.title('Confusion Matrix (TF-IDF)')
+plt.title('Confusion Matrix')
 plt.show()
+
+# Realizar validação cruzada
+cv_scores = cross_val_score(naive_bayes_classifier, X_train_vectorized, y_train, cv=5)
+
+# Exibir os resultados da validação cruzada
+print("Acurácia da validação cruzada:", cv_scores)
+print("Acurácia média da validação cruzada:", cv_scores.mean())
