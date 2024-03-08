d8e8f40c45c7155a62c181e0a3112094e8731734
adena-welt <vchristina122@gmail.com>
2024-03-07 22:37:42 -0300

    tf-idf, accuracy 0.97

--- smv.py
+++ smv.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import joblib
 
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
@@ -35,7 +35,7 @@ print("Fake:", test_fake_count)
 print("True:", test_true_count)
 
 # Vetorização do texto usando CountVectorizer
-vectorizer = CountVectorizer()
+vectorizer = TfidfVectorizer()
 X_train_vectorized = vectorizer.fit_transform(X_train)
 X_test_vectorized = vectorizer.transform(X_test)
 
